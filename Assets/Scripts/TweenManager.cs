c402c472850e19c999b3f7e9284bccbd88060748
Ahmet E. Dag <adag21044@gmail.com>
2024-08-17 20:30:44 +0300

    Initialize

---
+++ Assets/Scripts/TweenManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using DG.Tweening;
+
+public class TweenManager : MonoBehaviour
+{
+    public GameObject cube;
+    public GameObject sphere;
+    public GameObject capsule;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
